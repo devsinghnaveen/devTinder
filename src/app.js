751b982edc46d96382e8c94d7ad2227b25f79ec4
Naveen Singh <singhnaveen01june@gmail.com>
2025-09-14 19:17:37 +0530

    created an express server

---
+++ src/app.js
@@ -0,0 +1,30 @@
+
+// <---------- Creating  backend server ------------------->
+
+const express = require('express');
+
+const app = express();
+
+app.use('/hello', (req, res) => {
+    res.send('Hello from the server')
+})
+
+app.use('/test', (req, res) => {
+    res.send('Test from the server')
+})
+
+app.use('/', (req, res) => {
+    res.send('Home page from the server')
+})
+
+
+app.listen(3000, () => {
+    console.log('Server is successfully listening on port 3000')
+})
+
+
+
+
+
+
+
